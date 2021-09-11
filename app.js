96920f8982ebf7ffe3afab42319691e22b7c375c
ramincse <ramin.cse2009@gmail.com>
2021-09-11 21:45:36 +0600

    Conditional Rendering Done

--- app.js
+++ app.js
@@ -6,5 +6,13 @@ new Vue({
         link: "https://www.google.com.bd/",
         myText: "Hello Luffy",
         myHtml: "<strong>Hello Ruhul</strong>",
+        user: "Luffy",
+        userAge: 16,
+        allowedAge: 18,
+    },
+    methods: {
+        changeUser() {
+            return this.user === 'Luffy' ? true : false;
+        }
     }
 });
