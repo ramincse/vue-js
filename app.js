dc229e21c6ebb24fb37d186a3a03f1b5acc309ed
ramincse <ramin.cse2009@gmail.com>
2021-09-11 21:33:16 +0600

    Directives v-text and v-html Done

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ new Vue({
         src: "https://picsum.photos/id/237/200/300",
         alt: "This is a dog",
         link: "https://www.google.com.bd/",
+        myText: "Hello Luffy",
+        myHtml: "<strong>Hello Ruhul</strong>",
     }
 });
