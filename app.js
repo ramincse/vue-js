32e3f11b798f05183990ae3c591625f9d179a2de
ramincse <ramin.cse2009@gmail.com>
2021-09-12 10:03:49 +0600

    v-model Done

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
 new Vue({
     el: "#app",
     data: {
-        name: "Luffy",
-        x: 0,
-        y: 0,
+        formData: {
+            firstName: '',
+            lastName: '',
+        }
     },
     methods: {
-        updateName(newname, event) {
-            this.name = newname;
-            console.log(event);
-        },
-        getCoord(event) {
-            this.x = event.clientX;
-            this.y = event.clientY;
-        },
-        handleForm(event) {
-            console.log(event);
+        handleForm() {
+            console.log(this.formData);
         }
     },
 });
