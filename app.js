b169b296f56cf379217cd53b8f67c2822f67009c
ramincse <ramin.cse2009@gmail.com>
2021-09-12 10:15:58 +0600

    Computed Property Done

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 new Vue({
     el: "#app",
     data: {
-        formData: {
-            firstName: '',
-            lastName: '',
-        }
+        a: 0,
+        b: 0,
+        salary: 10,
     },
     methods: {
-        handleForm() {
-            console.log(this.formData);
-        }
+        
     },
+    computed: {
+        addToA() {
+            console.log('Add A');
+            return this.a + this.salary;
+        },
+
+        addToB() {
+            console.log('Add B');
+            return this.b + this.salary;
+        }
+    }
 });
